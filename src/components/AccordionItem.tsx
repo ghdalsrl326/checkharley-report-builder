9767b8b09638911ccb78a9a816e643c8944b6e71
minkihong <ghdalsrl326@gmail.com>
2023-06-18 21:00:25 +0900

    전기장치 서브타이틀 추가

--- src/components/AccordionItem.tsx
+++ src/components/AccordionItem.tsx
@@ -14,11 +14,25 @@ const AccordionItem = ({ title, measure, condition, variant }: Props) => {
       ? "bg-gray-100"
       : "bg-white";
 
+  const rangeSubTitle = (title: string): string => {
+    if (title === "최초 배터리 전압") {
+      return "정상범위 : 12.0V~13.0V";
+    } else if (title === "충전 전압") {
+      return "정상범위 : 13.0V 이상";
+    } else if (title === "엔진 쿨다운(15분) 전압 회복") {
+      return "정상범위 : 12.0V~13.0V";
+    }
+    return "";
+  };
+
   return (
     <span
       className={`flex flex-row items-center justify-between w-full aspect-[14/1] pl-8 pr-8 rounded-xl ${backgroundColor} object-contain text-lg text-black`}
     >
-      <h4>{title}</h4>
+      <div className="flex flex-row gap-4">
+        <h4>{title}</h4>
+        <h5 className="text-gray-500">{rangeSubTitle(title)}</h5>
+      </div>
       <div className="flex flex-row items-center justify-between gap-4 font-bold">
         <h4>{measure}</h4>
         <RightArrowIcon />
