f3100e3100fa3a0178c5916e89108396df30cda0
minkihong <ghdalsrl326@gmail.com>
2023-06-25 20:15:09 +0900

    add sanity studio vercel deployment plugin

--- sanity-studio/sanity.config.ts
+++ sanity-studio/sanity.config.ts
@@ -3,6 +3,7 @@ import {visionTool} from '@sanity/vision'
 import {deskTool} from 'sanity/desk'
 import {schemaTypes} from './schemas'
 import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+import {vercelDeployTool} from 'sanity-plugin-vercel-deploy'
 
 const devOnlyPlugins = [getStartedPlugin()]
 
@@ -13,10 +14,9 @@ export default defineConfig({
   projectId: '6f17ig6j',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+  plugins: [deskTool(), visionTool(), vercelDeployTool(), ...(isDev ? devOnlyPlugins : [])],
 
   schema: {
     types: schemaTypes,
   },
 })
-
