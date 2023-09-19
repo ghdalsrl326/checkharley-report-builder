8d14ec5d8e7f034129f4c6f4a1022928794e14aa
minkihong <ghdalsrl326@gmail.com>
2023-09-19 18:11:18 +0900

    [GUI] 블로그 페이지 반응형

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -7,7 +7,7 @@ const BlogPage = async () => {
   const previews = await getAllBlogPostPreview();
 
   return (
-    <div className="pl-44 pr-36 pt-40 pb-44 sm:p-4 md:p-16 lg:p-24">
+    <div className="p-4 sm:p-16 md:p-16 lg:p-24">
       <h1 className="text-3xl font-bold mb-8">블로그</h1>
       <Tabs posts={previews} />
     </div>
--- src/components/blog/Tabs.tsx
+++ src/components/blog/Tabs.tsx
@@ -8,8 +8,8 @@ const Tabs = ({ posts }: { posts: BlogPostPreviewList }) => {
   const [activeTab, setActiveTab] = useState("전체");
 
   return (
-    <div>
-      <div className="flex text-2xl font-medium relative mb-16">
+    <div className="w-full">
+      <div className="flex font-medium relative mb-16 text-lg sm:text-lg md:text-xl lg:text-2xl">
         <div className="absolute bottom-0 left-0 right-0 h-0.5 bg-[#D6D6D6]" />
         {BLOGTABS.map((tab) => (
           <button
