cd313b53bf6ea28769c7ddbd819dbcd911db1fe7
minkihong <ghdalsrl326@gmail.com>
2023-09-22 18:47:23 +0900

    [Package] nodemailer 설치

--- src/app/reservation/page.tsx
+++ src/app/reservation/page.tsx
@@ -8,7 +8,7 @@ const ReservationPage = () => {
       <div className="flex flex-col justify-center gap-8 relative h-screen px-6 md:pl-28 z-10">
         <h1 className="text-3xl md:text-5xl font-bold">점검 예약하기</h1>
         <h2 className="text-lg font-medium">
-          {`체크할리 정비사가 판매자에게 점검 동의를 구한 후 보고서를 작성해 고객님에게 보내드립니다`}
+          {`체크할리가 판매자에게 점검 동의를 구한 후 보고서를 작성해 고객님에게 보내드립니다`}
         </h2>
         <ReservationForm />
       </div>
